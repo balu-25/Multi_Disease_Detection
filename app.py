e03d254445d4cbf0f3a0d8e00330312a4ead0848
balu-25 <141560077+balu-25@users.noreply.github.com>
2025-12-23 10:38:59 +0530

    Update Flask app run configuration
    Change app.run to listen on all interfaces and set debug to False.

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def heart():
 def lungcancer():
     return render_template("lung_cancer_detection.html")
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
